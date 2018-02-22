050fc9d9370d8e38c32c3a0b558701f1f8ecfca4
Serge <vspox@yandex.ru>
2018-02-22 05:53:16 +0200

    included all .js files in main.js

--- dist/js/input-mask.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    $("#phone-client").mask("+7 (999) 999-99-99");
-    $("#help-doc-phone").mask("+7 (999) 999-99-99");
-    $('#callback-tel').mask("+7 (999) 999-99-99");
-});
